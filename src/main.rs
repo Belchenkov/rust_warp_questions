e5f7a313e3dc9e9f8ca6a00fabc6b8b6c12732d6
Алексей Бельченков <u608110@gmail.com>
2023-04-12 18:03:13 +0400

    Handle errors gracefully

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 use std::io::{Error, ErrorKind};
-use warp::Filter;
+use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
+use serde::Serialize;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -10,7 +11,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -44,13 +45,54 @@ impl FromStr for QuestionId {
     }
 }
 
-#[tokio::main]
-async fn main() {
+#[derive(Debug)]
+struct InvalidId;
+
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "First Question".to_string(),
         "Content of question".to_string(),
         Some(vec!("faq".to_string())),
     );
-    println!("{:?}", question);
+
+    match question.id.0.parse::<i32>() {
+        Err(_) =>  {
+            Err(warp::reject::custom(InvalidId))
+        },
+        Ok(_) => {
+            Ok(warp::reply::json(&question))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(_InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items;
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
