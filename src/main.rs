c1904254395a4dd2b7eb09c9ae52129425c92784
Алексей Бельченков <u608110@gmail.com>
2023-04-22 15:48:38 +0400

    handle errors lib

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use warp::{
     http::Method,
 };
 
+mod error;
+
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 struct AnswerId(String);
 
@@ -50,63 +52,6 @@ struct Question {
 
 #[derive(Deserialize, Debug, Serialize, Clone, PartialEq, Eq, Hash)]
 struct QuestionId(String);
-
-mod error {
-    use warp::{
-        filters::{
-            body::BodyDeserializeError,
-            cors::CorsForbidden,
-        },
-        reject::Reject,
-        Rejection,
-        Reply,
-        http::StatusCode,
-    };
-
-    #[derive(Debug)]
-    pub enum Error {
-        ParseError(std::num::ParseIntError),
-        MissingParameters,
-        QuestionNotFound,
-    }
-
-    impl Reject for Error {}
-
-    impl std::fmt::Display for Error {
-        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            match *self {
-                Error::ParseError(ref err) => write!(f, "Cannot parse parameter: {}", err),
-                Error::MissingParameters => write!(f, "Missing parameter"),
-                Error::QuestionNotFound => write!(f, "Question not found"),
-            }
-        }
-    }
-
-   pub async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-        if let Some(error) = r.find::<Error>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::RANGE_NOT_SATISFIABLE,
-            ))
-        } else if let Some(error) = r.find::<CorsForbidden>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::FORBIDDEN,
-            ))
-        } else if let Some(error) = r.find::<BodyDeserializeError>() {
-            Ok(warp::reply::with_status(
-                error.to_string(),
-                StatusCode::UNPROCESSABLE_ENTITY,
-            ))
-        } else {
-            Ok(warp::reply::with_status(
-                "Route not found".to_string(),
-                StatusCode::NOT_FOUND,
-            ))
-        }
-    }
-}
-
 
 #[derive(Debug)]
 struct Pagination {
@@ -201,7 +146,7 @@ async fn delete_question(id: String, store: Store) -> Result<impl warp::Reply, w
     }
 }
 
-fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, error::Error> {
+fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, error::error::Error> {
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagination {
             start: params
